4cd57149e2674710eb9fea010d0e50c883f33935
Shashidhara K <shashidharak334@gmail.com>
2026-09-18 22:36:14 +0530

    Initial commit

---
+++ src/main/java/com/reserve/backend/ReserveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reserve.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveBackendApplication.class, args);
+	}
+
+}
